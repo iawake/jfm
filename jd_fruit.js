797231b5f9bd063861017b2e62cb91bedac3861e
i-awake <73976513+i-awake@users.noreply.github.com>
2020-11-12 14:11:25 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -29,12 +29,12 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'caaa869cc54644d8b7d2b4a5ec812226cc9e@caaa2222869cc54644d8b7d2b4a5ec86cc9e@caa222a869cc54644d8b7d2b4a5ec86cc9e',
+  'e5a52fb7710540d38810267f84e52531@e5a52fb7710540d38810267f84e52531@e5a52fb7710540d38810267f84e52531',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'caaa869cc522334644d8b7d2b4a5ec86cc9e@caaa869cc54644d8b22217d2b4a5ec86cc9e@caaa869cc22254644d8b7d2b4a5ec86cc9e',
+  'e5a52fb7710540d38810267f84e52531@e5a52fb7710540d38810267f84e52531@e5a52fb7710540d38810267f84e52531',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
-const retainWater = 100;//保留水滴大于多少g,默认100g;
+const retainWater = 1000;//保留水滴大于多少g,默认100g;
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
 let jdFruitBeanCard = false;//农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
 let randomCount = 20;
