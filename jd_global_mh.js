5a37fe6c9fc697e6dfd6baa5c34101d41cdb4a6e
i-awake <73976513+i-awake@users.noreply.github.com>
2021-02-13 23:32:24 +0800

    Update jd_global_mh.js

--- jd_global_mh.js
+++ jd_global_mh.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 京东国际盲盒
 活动时间:2021-01-15至2021-02-15
 暂不加入品牌会员
@@ -10,11 +10,14 @@
 [task_local]
 #京东国际盲盒
 0 9,12,20,21 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_global_mh.js, tag=京东国际盲盒, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/main/Icon/lxk0301/jd_global_mh.png, enabled=true
+
 ================Loon==============
 [Script]
 cron "0 9,12,20,21 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_global_mh.js,tag=京东国际盲盒
+
 ===============Surge=================
 京东国际盲盒 = type=cron,cronexp="0 9,12,20,21 * * *",wake-system=1,timeout=3600,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_global_mh.js
+
 ============小火箭=========
 京东国际盲盒 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_global_mh.js, cronexpr="0 9,12,20,21 * * *", timeout=3600, enable=true
  */
